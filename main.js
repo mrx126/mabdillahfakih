21e9984b794c4ae32b5ba0f619e83a51a40538c8
mrx126 <mirzaabdillahfakih@gmail.com>
2023-12-08 19:25:46 +0700

    Menambahkan perubahan baru, menghapus beberapa file, dan memodifikasi beberapa file

--- main.js
+++ main.js
@@ -7,25 +7,56 @@ window.addEventListener("scroll", () => {
 
 //Filter (memilih kategori)
 // $(document).ready(function () {
-//   $(".filter-item").click(function () {
-//     const value = $(this).attr("data-filter");
-//     if (value == "all") {
-//       $(".post-box").show("1000");
-//     } else {
-//       $(".post-box")
-//         .not("." + value)
-//         .hide(1000);
-//       $(".post-box")
-//         .filter("." + value)
-//         .show("1000");
-//     }
-//   });
-//   $(".filter-item").click(function () {
-//     $(this).addClass("active-filter").siblings().removeClass("active-filter");
-//   });
+var filter = document.querySelectorAll(".filter");
+filter.forEach((n) =>
+  n.addEventListener("click", function () {
+    // console.log(filter);
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".postBox").show("1000");
+      var title = document.querySelector(".title.fx");
+      title.style.display = "flex";
+      var sembunyi = document.querySelectorAll(".sembunyi");
+      // sembunyi.forEach((n) => (n.style.backgroundColor = "red"));
+      sembunyi.forEach((n) => (n.style.display = "block"));
+      // sembunyi.forEach((n) => (n.style.display = "flex"));
+      const btn = document.querySelector(".btnMuncul");
+      btn.value = "Sembunyikan";
+    } else {
+      $(".postBox")
+        .not("." + value)
+        .hide(1000);
+      $(".postBox")
+        .filter("." + value)
+        .show("1000");
+      var title = document.querySelector(".title.fx");
+      title.style.display = "none";
+    }
+  })
+);
 // });
+// sembunyi
+const btn = document.querySelector(".btnMuncul");
+btn.value = "Sembunyikan";
+btn.addEventListener("click", function () {
+  if (btn.value === "Sembunyikan") {
+    var sembunyi = document.querySelectorAll(".sembunyi");
+    // sembunyi.forEach((n) => (n.style.backgroundColor = "blue"));
+    sembunyi.forEach((n) => (n.style.display = "none"));
+    // sembunyi.forEach((n) => (n.style.display = "none"));
+    const btn = document.querySelector(".btnMuncul");
+    btn.value = "lebih banyak";
+  } else {
+    var sembunyi = document.querySelectorAll(".sembunyi");
+    // sembunyi.forEach((n) => (n.style.backgroundColor = "red"));
+    sembunyi.forEach((n) => (n.style.display = "block"));
+    // sembunyi.forEach((n) => (n.style.display = "flex"));
+    const btn = document.querySelector(".btnMuncul");
+    btn.value = "Sembunyikan";
+  }
+});
 
-// home (efek parallax
+// home (efek parallax)
 $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
 
@@ -43,57 +74,35 @@ $(window).scroll(function () {
   }
 });
 
+// // sembunyi
+// const btn = document.querySelector(".btnMuncul");
+// btn.addEventListener("click", function () {
+//   var sembunyi = document.querySelectorAll(".sembunyi");
+//   sembunyi.forEach((n) => n.classList.toggle("ba"));
+//   // var tengah = document.querySelectorAll(".tengah");
+//   // tengah.forEach((n) => n.classList.toggle("extraHeight"));
+//   // console.log(tengah);
+
+//   if (btn.value === "lebih banyak") {
+//     btn.value = "Sembunyikan";
+//   } else {
+//     btn.value = "lebih banyak";
+//   }
+// });
+
 // sembunyi
+// const btn = document.querySelector(".btnMuncul");
+// btn.value = "Sembunyikan";
+// btn.addEventListener("click", function () {
+//   var sembunyi = document.querySelectorAll(".sembunyi");
+//   sembunyi.forEach((n) => n.classList.toggle("ba"));
+//   // var tengah = document.querySelectorAll(".tengah");
+//   // tengah.forEach((n) => n.classList.toggle("extraHeight"));
+//   // console.log(tengah);
 
-const btn = document.querySelector(".btnMuncul");
-btn.addEventListener("click", function () {
-  // document.querySelectorAll(".sembunyi").forEach((element) => {
-  //   element.toggle.style.display = "none";
-  // });
-  var sembunyi = document.querySelectorAll(".sembunyi");
-  sembunyi.forEach((n) => n.classList.toggle("ba"));
-  var tengah = document.querySelectorAll(".tengah");
-
-  // belum selesai sampai bawah
-  // tengah.forEach((n) => {
-  //   n.classList.toggle("extraHeight");
-  // });
-  tengah.forEach((n) => n.classList.toggle("extraHeight"));
-  // if (tengah.src === "images/post6.jpg") {
-  //   tengah.src === "images/post4.jpg";
-  // } else {
-  //   tengah.src === "images/post6.jpg";
-  // }
-  console.log(tengah);
-  // tengah.forEach((n) => n.classList.toggle("extraHeight"));
-
-  // let gambar1 = document.querySelector(".gambar1");
-  // let gambar2 = document.querySelector(".gambar2");
-
-  // console.log(value1);
-  // console.log(value2);
-
-  // [value1, value2] = [value2, value1];
-
-  // console.log(value1);
-  // console.log(value2);
-  // if ((gambar1.src = "images/post6.jpg")) {
-  //   gambar1.src = "images/post4.jpg";
-  // } else {
-  //   gambar1.src = "images/post6.jpg";
-  // }
-  // if ((gambar2.src = "images/post4.jpg")) {
-  //   gambar2.src = "images/post6.jpg";
-  // } else {
-  //   gambar2.src = "images/post4.jpg";
-  // }
-  // value1.src = "images/post6.jpg";
-  // value2.src = "images/post6.jpg";
-
-  if (btn.value === "lebih banyak") {
-    btn.value = "Sembunyikan";
-  } else {
-    btn.value = "lebih banyak";
-  }
-  // console.log(btn);
-});
+//   if (btn.value === "lebih banyak") {
+//     btn.value = "Sembunyikan";
+//   } else {
+//     btn.value = "lebih banyak";
+//   }
+// });
